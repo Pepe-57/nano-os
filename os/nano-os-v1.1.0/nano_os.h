916513506a393af31a7eb98a5c66ce79b3b3ee12
Pepe57 <104223887+Pepe-57@users.noreply.github.com>
2024-03-31 01:25:20 +0200

    Created nano_os.h at nano-os-v1.1.0

---
+++ os/nano-os-v1.1.0/nano_os.h
@@ -0,0 +1,13 @@
+#ifndef NANO_OS_H
+#define NANO_OS_H
+
+#define OS_VERSION "1.1.0"
+
+class operating_system {
+public:
+    void terminal();
+    void calculator();
+};
+
+extern operating_system OperatingSystem;
+#endif
